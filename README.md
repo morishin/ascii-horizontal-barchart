8388fe5e6d35cc6c5af99828f6a6b1bb87445215
Shintaro Morikawa <sntr92@gmail.com>
2016-07-04 23:08:21 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ascii-horizontal-barchart
+
+```
+npm install ascii-horizontal-barchart
+```
+
+```javascript
+const data = {
+  Ruby: 39146,
+  C: 18768,
+  Python: 268799,
+  HTML: 2597,
+  CSS: 3196,
+  Haskell: 23832,
+  Shell: 19098,
+  JavaScript: 6422
+}
+
+const chart = require('ascii-horizontal-barchart')
+console.log(chart(data))
+```
+
+![](https://cloud.githubusercontent.com/assets/1413408/16563178/81fd1a88-423b-11e6-91c6-a55aa321a77f.png)
---
+++ chart.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const chart = (data, maxLength = 100) => {
+  const formatted = Object.keys(data).map((key) => { return {key:key, value:data[key]} });
+  const sorted = formatted.sort((a, b) => b.value - a.value);
+  const maxValue = Math.max(...sorted.map((item) => item.value));
+  const maxKeyLength = Math.max(...sorted.map((item) => item.key.length));
+  return sorted.map((item) => {
+    const barLength = Math.round(item.value * maxLength / maxValue);
+    return item.key + " ".repeat(maxKeyLength - item.key.length + 1)
+            + "█".repeat(barLength);
+  }).join('\n');
+}
+
+module.exports = chart;
