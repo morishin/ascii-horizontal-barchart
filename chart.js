4599e05a83242dbe9c156f638130d0f2b488fced
Shintaro Morikawa <sntr92@gmail.com>
2016-07-05 23:36:50 +0900

    Make bar length zero only when value equals 0

--- chart.js
+++ chart.js
@@ -1,13 +1,14 @@
 "use strict";
 
 const bar = (value, maxValue, maxBarLength) => {
-  const fractions = ['', '▏', '▎', '▍', '▋', '▊', '▉']
-  const barLength = value * maxBarLength / maxValue
-  const wholeNumberPart = Math.floor(barLength)
-  const fractionalPart = barLength - wholeNumberPart
-  let bar = fractions[fractions.length - 1].repeat(barLength)
-  bar += fractions[Math.floor(fractionalPart * fractions.length)]
-  return bar
+  const fractions = ['▏', '▎', '▍', '▋', '▊', '▉'];
+  const barLength = value * maxBarLength / maxValue;
+  const wholeNumberPart = Math.floor(barLength);
+  const fractionalPart = barLength - wholeNumberPart;
+  let bar = fractions[fractions.length - 1].repeat(wholeNumberPart);
+  if (fractionalPart > 0)
+    bar += fractions[Math.floor(fractionalPart * fractions.length)];
+  return bar;
 }
 
 const chart = (data, dataLabel = false, maxBarLength = 100) => {
@@ -18,8 +19,8 @@ const chart = (data, dataLabel = false, maxBarLength = 100) => {
   return sorted.map((item) => {
     const prefix = item.key + " ".repeat(maxKeyNameLength - item.key.length + 1);
     const barText = bar(item.value, maxValue, maxBarLength);
-    const suffix = dataLabel ? ` ${item.value}` : ""
-    return prefix + barText + suffix
+    const suffix = dataLabel ? ` ${item.value}` : "";
+    return prefix + barText + suffix;
   }).join('\n');
 }
 
