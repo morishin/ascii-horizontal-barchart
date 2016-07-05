27f6d99c7b10b07040d74fd310e5f3078359b1dd
shintaro-morikawa <sntr92@gmail.com>
2016-07-05 13:27:32 +0900

    Use various width square characters

--- chart.js
+++ chart.js
@@ -1,14 +1,25 @@
 "use strict";
 
-const chart = (data, dataLabel = false, maxLength = 100) => {
+const bar = (value, maxValue, maxBarLength) => {
+  const fractions = ['', '▏', '▎', '▍', '▋', '▊', '▉']
+  const barLength = value * maxBarLength / maxValue
+  const wholeNumberPart = Math.floor(barLength)
+  const fractionalPart = barLength - wholeNumberPart
+  let bar = fractions[fractions.length - 1].repeat(barLength)
+  bar += fractions[Math.floor(fractionalPart * fractions.length)]
+  return bar
+}
+
+const chart = (data, dataLabel = false, maxBarLength = 100) => {
   const formatted = Object.keys(data).map((key) => { return {key:key, value:data[key]} });
   const sorted = formatted.sort((a, b) => b.value - a.value);
   const maxValue = Math.max(...sorted.map((item) => item.value));
-  const maxKeyLength = Math.max(...sorted.map((item) => item.key.length));
+  const maxKeyNameLength = Math.max(...sorted.map((item) => item.key.length));
   return sorted.map((item) => {
-    const barLength = Math.round(item.value * maxLength / maxValue);
-    return item.key + " ".repeat(maxKeyLength - item.key.length + 1)
-            + "█".repeat(barLength) + (dataLabel ? ` ${item.value}` : "");
+    const prefix = item.key + " ".repeat(maxKeyNameLength - item.key.length + 1);
+    const barText = bar(item.value, maxValue, maxBarLength);
+    const suffix = dataLabel ? ` ${item.value}` : ""
+    return prefix + barText + suffix
   }).join('\n');
 }
 
