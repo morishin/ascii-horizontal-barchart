9ee6c3732898433791257aebb8e3f587e0378bac
Shintaro Morikawa <sntr92@gmail.com>
2016-07-09 22:19:21 +0900

    Format

--- src/chart.js
+++ src/chart.js
@@ -12,11 +12,11 @@ const bar = (value, maxValue, maxBarLength) => {
 }
 
 const chart = (data, showValue = false, maxBarLength = 100) => {
-  const formatted = Object.keys(data).map((key) => { return { key: key, value: data[key] } });
+  const formatted = Object.keys(data).map(key => ({ key: key, value: data[key] }));
   const sorted = formatted.sort((a, b) => b.value - a.value);
-  const maxValue = Math.max(...sorted.map((item) => item.value));
-  const maxKeyNameLength = Math.max(...sorted.map((item) => item.key.length));
-  return sorted.map((item) => {
+  const maxValue = Math.max(...sorted.map(item => item.value));
+  const maxKeyNameLength = Math.max(...sorted.map(item => item.key.length));
+  return sorted.map(item => {
     const prefix = item.key + " ".repeat(maxKeyNameLength - item.key.length + 1);
     const barText = bar(item.value, maxValue, maxBarLength);
     const suffix = showValue ? ` ${item.value}` : "";
