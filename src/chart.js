07e3ff543595e615c88230eebab89f0ed5a2c105
Shintaro Morikawa <sntr92@gmail.com>
2016-07-09 19:45:11 +0900

    Update parameter name

--- src/chart.js
+++ src/chart.js
@@ -11,15 +11,15 @@ const bar = (value, maxValue, maxBarLength) => {
   return bar;
 }
 
-const chart = (data, dataLabel = false, maxBarLength = 100) => {
-  const formatted = Object.keys(data).map((key) => { return {key:key, value:data[key]} });
+const chart = (data, showValue = false, maxBarLength = 100) => {
+  const formatted = Object.keys(data).map((key) => { return { key: key, value: data[key] } });
   const sorted = formatted.sort((a, b) => b.value - a.value);
   const maxValue = Math.max(...sorted.map((item) => item.value));
   const maxKeyNameLength = Math.max(...sorted.map((item) => item.key.length));
   return sorted.map((item) => {
     const prefix = item.key + " ".repeat(maxKeyNameLength - item.key.length + 1);
     const barText = bar(item.value, maxValue, maxBarLength);
-    const suffix = dataLabel ? ` ${item.value}` : "";
+    const suffix = showValue ? ` ${item.value}` : "";
     return prefix + barText + suffix;
   }).join('\n');
 }
